b46bbc8ef6b1137be017ac5735c79838a79f3298
arunkb2795 <arunkb27@gmail.com>
2026-05-08 15:45:18 +0530

    added pwa

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,20 +3,20 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "IBuildThis PWA",
-    short_name: "IBuildThisPWA",
-    description: "A Progressive Web App built with Next.js",
+    short_name: "IBuildThis",
+    description: "My Next.js 16 PWA",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/apple-icon-180.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/apple-icon-180.png",
         sizes: "512x512",
         type: "image/png",
       },
--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,19 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+// import withSerwistInit from "@serwist/next";
+
+// const withSerwist = withSerwistInit({
+//   swSrc: "src/sw.ts",
+//   swDest: "public/sw.js",
+//   disable: process.env.NODE_ENV === "development",
+// });
+
+// const nextConfig: NextConfig = {
+//   turbopack: {},
+//   cacheComponents: true,
+// };
+
+// export default withSerwist(nextConfig);
